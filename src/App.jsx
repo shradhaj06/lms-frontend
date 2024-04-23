2c310141568d61a46dbdf9ae3018a1f5215502e3
Shradha Jha <shradhajha0609@gmail.com>
2024-04-23 16:49:02 +0530

    Added signup and sign in functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom';
 import AboutUs from './pages/AboutUs';
 import NotFound from './pages/NotFound';
-
-
+import Signup from './pages/SignUp';
+import SignIn from './pages/SignIn';
 function App() {
-
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<AboutUs/>}/>
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/signin' element={<SignIn />} />
       <Route path="*" element={<NotFound/>}/>
     </Routes>
   );
--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,10 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit"
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import toast from "react-hot-toast";
+import axiosInstance from "../../config/axiosInstance";
 const initialState={
     isLoggedIn:localStorage.getItem("isLoggedIn")||false,
     role: localStorage.getItem("role")|| "",
     data: localStorage.getItem("data")||{}
 }
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+    try {
+        const response = axiosInstance.post("user/register", data);
+        toast.promise(response, {
+            loading: 'Wait! creating your account',
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: 'Failed to create your account'
+        });
+        return await response;
+    } catch(error) {
+        console.log(error);
+        toast.error(error?.response?.data?.message);
+    }
+})
+export const login = createAsyncThunk("/auth/signin", async (data) => {
+    try {
+        const response = axiosInstance.post("user/login", data);
+        toast.promise(response, {
+            loading: 'Wait! authenticating your account',
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: 'Failed to authenticate your account'
+        });
+        return await response;
+    } catch(error) {
+        console.log(error);
+        toast.error(error?.response?.data?.message);
+    }
+})
+
 const authSlice =createSlice({
     name:"auth",
     initialState,
